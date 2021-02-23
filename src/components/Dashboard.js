f1a987db634dc11b42a89371bc8129cd85798a8f
manjunathangv <54570517+manjunathangv@users.noreply.github.com>
2021-02-23 23:32:26 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,52 +5,13 @@ import { Container } from "@material-ui/core";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      todos: [
-        {
-          date: 28,
-          month: "may",
-          title: "Revamp Instgram app"
-        },
-        {
-          date: 29,
-          month: "may",
-          title: "Revamp Instgram app"
-        },
-        {
-          date: 30,
-          month: "Jan",
-          title: "Revamp Instgram app"
-        }
-      ]
-    };
+    this.state = {};
   }
-  addTodo = () => {
-    console.log("added");
-    let newtodos = this.state.todos;
-    newtodos.unshift({
-      date: 31,
-      month: "Dec",
-      title: "Revamp Gpay  app"
-    });
-    this.setState({ todos: newtodos });
-  };
-  removetodo = index => {
-    console.log("remove todo" + index);
-    let newtodos = this.state.todos.filter((item, i) => {
-      return index !== i;
-    });
-    this.setState({ todos: newtodos });
-  };
   render() {
     return (
       <Container className="mainContainer">
         <Leftpanel />
-        <RightPanel
-          todos={this.state.todos}
-          addtodo={this.addTodo}
-          removetodo={this.removetodo}
-        />
+        <RightPanel />
       </Container>
     );
   }
